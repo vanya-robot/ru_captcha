9b7e5fe38c561cacc655d65b1a4dad5926e1f305
vanya-robot <alexeenkoyaroslav@mail.ru>
2022-10-07 20:52:34 +0300

    Add generate_arrays func to data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,41 @@
-# Добавить классы и прочие функции в разные файлы для удобства
+import cv2
+import numpy as np
+from data_generator import is_valid_captcha
+
+# Store arrays in memory as it's not a muv big dataset
+
+
+def generate_arrays(df, resize=True, img_height=60, img_width=200):
+    """Generates image array and labels array from a dataframe.
+
+    Args:
+        df: dataframe from which we want to read the data
+        resize (bool)    : whether to resize images or not
+        img_width (int): width of the resized images
+        img_height (int): height of the resized images
+
+    Returns:
+        images (ndarray): grayscale images
+        labels (ndarray): corresponding encoded labels
+    """
+
+    num_items = len(df)
+    images = np.zeros((num_items, img_height, img_width), dtype=np.float32)
+    labels = [0]*num_items
+
+    for i in range(num_items):
+        img = cv2.imread(df["img_path"][i])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        if resize:
+            img = cv2.resize(img, (img_width, img_height))
+
+        img = (img/255.).astype(np.float32)
+        label = df["label"][i]
+
+        # Add only if it is a valid captcha
+        if is_valid_captcha(label):
+            images[i, :, :] = img
+            labels[i] = label
+
+    return images, np.array(labels)
